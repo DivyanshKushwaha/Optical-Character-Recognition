7f63ae6d99094dda017efd6d29e1a20441eca204
Divyansh Kushwaha <jatinkushwaha309@gmail.com>
2024-09-14 18:05:09 +0530

    OCR and CNN model built

---
+++ models/model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense,Conv2D, MaxPooling2D,Flatten
+
+
+def cnn_model():
+    model = Sequential([
+        Conv2D(32,(3,3), activation='relu',input_shape= (224,224,1)),
+        MaxPooling2D(pool_size=(2,2)),
+        Conv2D(64,(3,3),activation='relu'),
+        MaxPooling2D(pool_size=(2,2)),
+        Flatten(),
+        Dense(128,activation='relu'),
+        Dense(len(allowed_units),activation='softmax')
+    ])
+    model.compile(optimizer='adam',
+                  loss = 'categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
